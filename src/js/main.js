ceba37fd9f8f03ef2aaf4d6de13839d6795a49e8
weterynarzfred <weterynarzfred@gmail.com>
2023-03-20 10:07:41 +0100

    switch to production

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,6 @@
-import $ from 'cash-dom';
-
-const letters = [];
-$('.letter').each((_index, DOMElement) => {
-  letters.push($(DOMElement));
-});
-
-const smallLetters = [];
-$('.letter-small').each((_index, DOMElement) => {
-  smallLetters.push($(DOMElement));
-});
-
-const key = $('#key');
+const letters = Array.from(document.getElementsByClassName('letter'));
+const smallLetters = Array.from(document.getElementsByClassName('letter-small'));
+const key = document.getElementById('key');
 
 let time = 500;
 function queue(callback, duration = 0) {
@@ -19,36 +9,11 @@ function queue(callback, duration = 0) {
 }
 
 window.addEventListener('load', () => {
-  queue(() => key.addClass('active'), 1000);
-  queue(() => key.css({ transition: '0.5s' }));
-
-  time += 200;
-
-  queue(() => {
-    key.addClass('active-2');
-  }, 500);
-
-  time -= 400;
-
-  letters.forEach(letter => {
-    queue(() => {
-      letter.toggleClass('active');
-    }, 400);
-    time -= 200;
-  });
-
+  queue(() => key.classList.add('active'), 800);
+  queue(() => key.style.transition = '0.5s');
+  queue(() => key.classList.add('active-2'), 100);
+  letters.forEach(letter => queue(() => letter.classList.add('active'), 200));
   time += 300;
-
-  queue(() => {
-    key.addClass('active-3');
-  }, 500);
-
-  time -= 300;
-
-  smallLetters.forEach(letter => {
-    queue(() => {
-      letter.toggleClass('active');
-    }, 200);
-    time -= 185;
-  });
+  queue(() => key.classList.add('active-3'), 200);
+  smallLetters.forEach(letter => queue(() => letter.classList.add('active'), 15));
 });
